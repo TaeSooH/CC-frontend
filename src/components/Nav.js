3a980b7ca7351fdb909e14b7eb8c918a34445ad0
LLTae2 <taehyun5820@gmail.com>
2022-07-23 02:38:30 +0900

    Profile 컴포넌트 완성, css 추가

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,14 +6,14 @@ import "../style/Main.css"
 const Nav = () => {
   return (
     <div className='Nav_box'>
-        <BsGithub className='Main_icon' size='30' color='black'/>
-        <p className='Main_title'>Commit Competition</p>
-        <div className='Main_link_bar'>
-            <div>Main</div>
-            <div>Ranking</div>
-        </div>
-        <img className='Nav_profile' src='https://avatars.githubusercontent.com/u/80657114?v=4' />
-        <VscTriangleDown className='Nav_profile_detail'/>
+      <BsGithub className='Main_icon' size='30' color='white'/>
+      <p className='Main_title'>Commit Competition</p>
+      <div className='Main_link_bar'>
+          <div>Main</div>
+          <div>Ranking</div>
+      </div>
+      <img className='Nav_profile' src='https://avatars.githubusercontent.com/u/80657114?v=4' />
+      <VscTriangleDown className='Nav_profile_detail' color='white'/>
     </div>
   )
 }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import "../style/Main.css"
+import { BsFillPeopleFill, BsDot } from 'react-icons/bs'
+import { BiBuilding } from "react-icons/bi"
+
 
 const Profile = () => {
   return (
-    <div></div>
+    <div className='Profile_wrap'>
+      <div className='Profile_box'>
+        <img src='https://avatars.githubusercontent.com/u/80657114?v=4' />
+        <h2>Taehyun Lee</h2>
+        <span>LLTae2</span>
+        <p>알고리즘을 해야 한다고 생각함</p>
+        <div className='Profile_follower'>
+          <BsFillPeopleFill className='Profile_icon' size='30' color='gray'/>
+          <span className='F_number'>15</span>
+          <span className='F_font'>followers</span>
+          <BsDot />
+          <span className='F_number'>15</span>
+          <span className='F_font'>following</span>
+        </div>
+        <div className='Profile_company'>
+          <BiBuilding className='Profile_icon' size='23' color='gray'/>
+          <p>Busan Software Meister High School</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
