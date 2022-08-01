178598d49e5f45998d5efde8546ba6862237be68
LLTae2 <taehyun5820@gmail.com>
2022-08-01 23:47:21 +0900

    MyTodayCommit component 생성

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -2,9 +2,42 @@ import React from 'react'
 import "../style/Main.css"
 
 const Graph = () => {
+
+
+
+  // const { data, barIndex, left, right, refAreaLeft, refAreaRight, top, bottom, top2, bottom2 } = this.state;
+
   return (
     <div className='Graph'>
+      {/* <div className="highlight-bar-charts" style={{ userSelect: 'none', width: '100%' }}>
+        <button type="button" className="btn update" onClick={this.zoomOut.bind(this)}>
+          Zoom Out
+        </button>
 
+        <ResponsiveContainer width="100%" height={400}>
+          <LineChart
+            width={800}
+            height={400}
+            data={data}
+            onMouseDown={(e) => this.setState({ refAreaLeft: e.activeLabel })}
+            onMouseMove={(e) => this.state.refAreaLeft && this.setState({ refAreaRight: e.activeLabel })}
+            // eslint-disable-next-line react/jsx-no-bind
+            onMouseUp={this.zoom.bind(this)}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis allowDataOverflow dataKey="name" domain={[left, right]} type="number" />
+            <YAxis allowDataOverflow domain={[bottom, top]} type="number" yAxisId="1" />
+            <YAxis orientation="right" allowDataOverflow domain={[bottom2, top2]} type="number" yAxisId="2" />
+            <Tooltip />
+            <Line yAxisId="1" type="natural" dataKey="cost" stroke="#8884d8" animationDuration={300} />
+            <Line yAxisId="2" type="natural" dataKey="impression" stroke="#82ca9d" animationDuration={300} />
+
+            {refAreaLeft && refAreaRight ? (
+              <ReferenceArea yAxisId="1" x1={refAreaLeft} x2={refAreaRight} strokeOpacity={0.3} />
+            ) : null}
+          </LineChart>
+        </ResponsiveContainer>
+      </div> */}
     </div>
   )
 }
---
+++ src/components/TodayCommit.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TodayCommit = () => {
+  return (
+    <div className='Today_commit_wrap'>
+        
+    </div>
+  )
+}
+
+export default TodayCommit
