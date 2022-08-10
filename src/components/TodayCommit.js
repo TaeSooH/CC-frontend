6134a1e0ae025dc76b800d0151e8c9e5abd56863
LLTae2 <taehyun5820@gmail.com>
2022-08-10 23:29:26 +0900

    Today commit 컴포넌트 내용 추가

--- src/components/TodayCommit.js
+++ src/components/TodayCommit.js
@@ -4,9 +4,16 @@ import "../style/Main.css"
 const TodayCommit = () => {
   return (
     <div className='Today_commit_wrap'>
-        <div className='Today_commit_box'> 
-          <p>Your Today's Commit :</p>
-          <span>숫자</span>
+        <div className='Today_commit_container'> 
+          {/* <p>Your Today's Commit :</p>
+          <span>숫자</span> */}
+          <div className='Yesterday_commit_box'>
+            <span>숫자</span>
+            <p>My yesterday's commits</p>
+          </div>
+          <div className='Today_commit_box'>
+            <span>숫자</span>
+            <p>My today's commits</p></div>
         </div>
     </div>
   )
