0893a43d48ae361390d5b804a08a06b871ecd1bb
LLTae2 <taehyun5820@gmail.com>
2022-08-15 20:41:41 +0900

    ranking page 박스작업, css 수정

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -2,23 +2,28 @@ import React from 'react'
 import DailyRank from '../components/DailyRank'
 import MonthlyRank from '../components/MonthlyRank'
 import TotallyRank from '../components/TotallyRank'
+import Nav from '../components/Nav'
 import "../style/Ranking.css"
 
 const Ranking = () => {
   return (
-    <div className='container'>
-        <div>
-            <p></p>
-            <DailyRank />
-        </div>
-        <div>
-            <p></p>
-            <MonthlyRank />
-        </div>
-        <div>
-            <p></p>
-            <TotallyRank />
-        </div>
+    <div className='Rank_container'>
+      <div className='Nav_fix'>
+        <Nav />
+      </div>
+      <div className='Empty_box'></div>
+      <div className='Each_rank_box'>
+        <p></p>
+        <DailyRank />
+      </div>
+      <div className='Each_rank_box'>
+        <p></p>
+        <MonthlyRank />
+      </div>
+      <div className='Each_rank_box'>
+        <p></p>
+        <TotallyRank />
+      </div>
     </div>
   )
 }
